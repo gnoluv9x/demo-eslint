9b274ef0e07fc1601d557e5884f9010aea2a07a6
longvd <vulongpt@gmail.com>
2022-02-12 15:37:23 +0700

    Keep calm and commit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p 
+        className="abc">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-                  className="App-link"
+          className="App-link"
           href="https://reactjs.org"
-              target="_blank"
+          target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
