ca410fbbe0ca78abf73ab72207235bef4bdebc0d
longvd <vulongpt@gmail.com>
2022-02-13 13:01:09 +0700

    commit by long1

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p 
-        className="abc">
+        <p className="abc">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
